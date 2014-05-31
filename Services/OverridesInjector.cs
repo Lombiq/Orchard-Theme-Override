4cecf8a6115abbbc1d01826f8127cbe4306d2836
Piedone <piedone@pyrocenter.hu>
2014-05-31 17:59:37 +0200

    Fixing that OverridesInjector caused DependencyResolutionException on shell startup

--- Services/OverridesInjector.cs
+++ Services/OverridesInjector.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Orchard.DisplayManagement.Implementation;
+using Orchard.Environment;
 using Orchard.Mvc;
 using Orchard.UI.Admin;
 using Orchard.UI.Resources;
@@ -14,21 +15,25 @@ namespace Piedone.ThemeOverride.Services
     public class OverridesInjector : IShapeDisplayEvents
     {
         private readonly IHttpContextAccessor _hca;
-        private readonly IResourceManager _resourceManager;
+        private readonly Work<IResourceManager> _resourceManagerWork;
         private readonly IThemeOverrideService _themeOverrideService;
 
 
+        // Injecting IResourceManager is necessary because on shell start in the end IShapeDisplayEvents are instantiated, thus this class
+        // as well. ResourceManager also implementing IUnitOfWorkDependency causes an Autofac DependencyResolutionException with the message
+        // "No scope with a Tag matching 'work' is visible from the scope in which the instance was requested.".
+        // See: https://orchard.codeplex.com/workitem/20738
         public OverridesInjector(
             IHttpContextAccessor hca,
-            IResourceManager resourceManager,
+            Work<IResourceManager> resourceManagerWork,
             IThemeOverrideService themeOverrideService)
         {
             _hca = hca;
-            _resourceManager = resourceManager;
+            _resourceManagerWork = resourceManagerWork;
             _themeOverrideService = themeOverrideService;
         }
-        
-		
+
+
         public void Displaying(ShapeDisplayingContext context)
         {
             if (context.ShapeMetadata.Type != "DocumentZone" || context.Shape.ZoneName != "Head") return;
@@ -37,10 +42,12 @@ namespace Piedone.ThemeOverride.Services
             if (httpContext == null) return;
             if (AdminFilter.IsApplied(httpContext.Request.RequestContext)) return;
 
+            var resourceManager = _resourceManagerWork.Value;
+
             var overrides = _themeOverrideService.GetOverrides();
             if (overrides.FaviconUri != null)
             {
-                _resourceManager.RegisterLink(new LinkEntry { Type = "image/x-icon", Rel = "shortcut icon", Href = overrides.FaviconUri.ToString() });
+                resourceManager.RegisterLink(new LinkEntry { Type = "image/x-icon", Rel = "shortcut icon", Href = overrides.FaviconUri.ToString() });
             }
 
             if (overrides.StylesheetUris.Any())
@@ -48,14 +55,14 @@ namespace Piedone.ThemeOverride.Services
                 foreach (var uri in overrides.StylesheetUris)
                 {
                     var url = uri.ToString();
-                    _resourceManager.Include("stylesheet", url, url);
+                    resourceManager.Include("stylesheet", url, url);
                 }
             }
 
             if (overrides.CustomStyles.Uri != null)
             {
                 var url = overrides.CustomStyles.Uri.ToString();
-                _resourceManager.Include("stylesheet", url, url);
+                resourceManager.Include("stylesheet", url, url);
             }
 
             if (overrides.HeadScriptUris.Any())
@@ -63,14 +70,14 @@ namespace Piedone.ThemeOverride.Services
                 foreach (var uri in overrides.HeadScriptUris)
                 {
                     var url = uri.ToString();
-                    _resourceManager.Include("script", url, url).AtHead();
+                    resourceManager.Include("script", url, url).AtHead();
                 }
             }
 
             if (overrides.CustomHeadScript.Uri != null)
             {
                 var url = overrides.CustomHeadScript.Uri.ToString();
-                _resourceManager.Include("script", url, url).AtHead();
+                resourceManager.Include("script", url, url).AtHead();
             }
 
 
@@ -79,14 +86,14 @@ namespace Piedone.ThemeOverride.Services
                 foreach (var uri in overrides.FootScriptUris)
                 {
                     var url = uri.ToString();
-                    _resourceManager.Include("script", url, url).AtFoot();
+                    resourceManager.Include("script", url, url).AtFoot();
                 }
             }
 
             if (overrides.CustomFootScript.Uri != null)
             {
                 var url = overrides.CustomFootScript.Uri.ToString();
-                _resourceManager.Include("script", url, url).AtFoot();
+                resourceManager.Include("script", url, url).AtFoot();
             }
         }
 
